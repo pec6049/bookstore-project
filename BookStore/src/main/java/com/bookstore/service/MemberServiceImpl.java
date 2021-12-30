4f09aa2758e80a377504116141f5ff350e7de1e6
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2021-12-30 22:03:56 +0900

    Add files via upload

---
+++ BookStore/src/main/java/com/bookstore/service/MemberServiceImpl.java
@@ -0,0 +1,33 @@
+package com.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.mapper.MemberMapper;
+import com.bookstore.model.MemberVO;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	MemberMapper membermapper;
+
+	/* 회원가입 */
+	@Override
+	public void memberJoin(MemberVO member) throws Exception {
+		membermapper.memberJoin(member);
+	}
+	
+	/* 아이디 중복 검사 */
+	@Override
+	public int idCheck(String memberId) throws Exception {
+		return membermapper.idCheck(memberId);
+	}
+	
+	/* 로그인 */
+    @Override
+    public MemberVO memberLogin(MemberVO member) throws Exception {
+        return membermapper.memberLogin(member);
+    }
+    
+}
---
+++ BookStore/src/test/java/com/bookstore/persistence/TimeMapperTests.java
@@ -0,0 +1,28 @@
+package com.bookstore.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bookstore.mapper.TimeMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class TimeMapperTests {
+
+	@Autowired
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		System.out.println(timeMapper.getTime1());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		System.out.println(timeMapper.getTime2());
+	}
+	
+}
