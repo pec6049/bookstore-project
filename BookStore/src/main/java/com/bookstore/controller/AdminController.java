7267da678cb4de7ef83c1d591ce13fd06ba62502
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2021-12-31 21:00:56 +0900

    url 맵핑 메서드 작성

--- BookStore/src/main/java/com/bookstore/controller/AdminController.java
+++ BookStore/src/main/java/com/bookstore/controller/AdminController.java
@@ -13,8 +13,32 @@ public class AdminController {
 
 	/* 관리자 메인 페이지 이동 */
     @RequestMapping(value="main", method = RequestMethod.GET)
-    public void adminMainGET() throws Exception{
+    public void adminMainGET() throws Exception {
         log.info("관리자 페이지 이동");
+    }
+    
+    /* 상품 등록 페이지 접속 */
+    @RequestMapping(value = "goodsManage", method = RequestMethod.GET)
+    public void goodsManageGET() throws Exception {
+    	log.info("상품 등록 페이지 접속");
+    }
+    
+    /* 상품 등록 페이지 접속 */
+    @RequestMapping(value = "goodsEnroll", method = RequestMethod.GET)
+    public void goodsEnrollGET() throws Exception {
+    	log.info("상품 등록 페이지 접속");
+    }
+    
+    /* 작가 등록 페이지 접속 */
+    @RequestMapping(value = "authorEnroll", method = RequestMethod.GET)
+    public void authorEnrollGET() throws Exception {
+    	log.info("작가 등록 페이지 접속");
+    }
+    
+    /* 작가 관리 페이지 접속 */
+    @RequestMapping(value = "authorManage", method = RequestMethod.GET)
+    public void authorManageGET() throws Exception {
+    	log.info("작가 관리 페이지 접속");
     }
     
 }
