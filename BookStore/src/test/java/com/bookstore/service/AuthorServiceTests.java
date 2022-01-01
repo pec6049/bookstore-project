cdf0763313b794595eba3059d3ba25fe5a482998
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2022-01-01 16:28:22 +0900

    AuthorService 테스트

---
+++ BookStore/src/test/java/com/bookstore/service/AuthorServiceTests.java
@@ -0,0 +1,28 @@
+package com.bookstore.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bookstore.model.AuthorVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class AuthorServiceTests {
+
+    @Autowired
+    private AuthorService service;
+    
+    @Test
+    public void authorEnrollTest() throws Exception {
+        AuthorVO author = new AuthorVO();
+        
+        author.setNationId("01");
+        author.setAuthorName("테스트");
+        author.setAuthorIntro("테스트 소개");
+        
+        service.authorEnroll(author);
+    }
+}
