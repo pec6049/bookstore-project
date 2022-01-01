bc962ab9937a564eaea0e35388b7750873975335
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2022-01-01 16:29:01 +0900

    작가등록 추가

--- BookStore/src/main/java/com/bookstore/controller/AdminController.java
+++ BookStore/src/main/java/com/bookstore/controller/AdminController.java
@@ -1,8 +1,13 @@
 package com.bookstore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bookstore.model.AuthorVO;
+import com.bookstore.service.AuthorService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -10,6 +15,9 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+    private AuthorService authorService;
 
 	/* 관리자 메인 페이지 이동 */
     @RequestMapping(value="main", method = RequestMethod.GET)
@@ -39,6 +47,17 @@ public class AdminController {
     @RequestMapping(value = "authorManage", method = RequestMethod.GET)
     public void authorManageGET() throws Exception {
     	log.info("작가 관리 페이지 접속");
+    }
+    
+    /* 작가 등록 */
+    @RequestMapping(value="authorEnroll.do", method = RequestMethod.POST)
+    public String authorEnrollPOST(AuthorVO author, RedirectAttributes rttr) throws Exception{
+    	log.info("authorEnroll :" +  author);
+        
+        authorService.authorEnroll(author);  //작가 등록 쿼리 수행
+        rttr.addFlashAttribute("enroll_result", author.getAuthorName());
+        
+        return "redirect:/admin/authorManage";
     }
     
 }
