fff960c2bad7fea6fb089f8c8d31d0e358d0d044
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2022-01-02 21:30:12 +0900

    작가 상세 페이지 테스트

--- BookStore/src/test/java/com/bookstore/mapper/AuthorMapperTests.java
+++ BookStore/src/test/java/com/bookstore/mapper/AuthorMapperTests.java
@@ -49,6 +49,7 @@ public class AuthorMapperTests {
     */
 	
 	/* 작가 총 수 */
+	/*
 	@Test
 	public void authorGetTotalTest() throws Exception {
 		Criteria cri = new Criteria();
@@ -58,5 +59,15 @@ public class AuthorMapperTests {
 		
 		System.out.println("total................" + total);
 	}
+	*/
 	
+	/* 작가 상세 페이지 */
+	@Test
+	public void authorGetDetailTest() {
+		int authorId = 30;
+		
+		AuthorVO author = mapper.authorGetDetail(authorId);
+		
+		System.out.println("author......." + author);
+	}
 }
