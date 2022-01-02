df99086f3db8abfe02bf635586498f3b2b5cb134
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2022-01-02 12:49:28 +0900

    작가 목록 추가

--- BookStore/src/main/java/com/bookstore/mapper/AuthorMapper.java
+++ BookStore/src/main/java/com/bookstore/mapper/AuthorMapper.java
@@ -1,9 +1,15 @@
 package com.bookstore.mapper;
 
+import java.util.List;
+
 import com.bookstore.model.AuthorVO;
+import com.bookstore.model.Criteria;
 
 public interface AuthorMapper {
 
 	/* 작가 등록 */
     public void authorEnroll(AuthorVO author);
+    
+    /* 작가 목록 */
+    public List<AuthorVO> authorGetList(Criteria cri);
 }
