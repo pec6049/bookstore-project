9a05567df4f06b028a4d283d2c2e3236e9a03f6e
PARK EunChae <78144748+pec6049@users.noreply.github.com>
2022-01-02 13:07:58 +0900

    작가 목록 추가

--- BookStore/src/main/java/com/bookstore/service/AuthorService.java
+++ BookStore/src/main/java/com/bookstore/service/AuthorService.java
@@ -1,10 +1,16 @@
 package com.bookstore.service;
 
+import java.util.List;
+
 import com.bookstore.model.AuthorVO;
+import com.bookstore.model.Criteria;
 
 public interface AuthorService {
 	
 	/* 작가 등록 */
     public void authorEnroll(AuthorVO author) throws Exception;
+    
+    /* 작가 목록 */
+    public List<AuthorVO> authorGetList(Criteria cri) throws Exception;
 
 }
--- BookStore/src/main/java/com/bookstore/service/AuthorServiceImpl.java
+++ BookStore/src/main/java/com/bookstore/service/AuthorServiceImpl.java
@@ -1,20 +1,34 @@
 package com.bookstore.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.mapper.AuthorMapper;
 import com.bookstore.model.AuthorVO;
+import com.bookstore.model.Criteria;
+
+import lombok.extern.log4j.Log4j;
 
 @Service
+@Log4j
 public class AuthorServiceImpl implements AuthorService {
 
 	@Autowired
     AuthorMapper authorMapper;
 	
+	/* 작가 등록 */
 	@Override
     public void authorEnroll(AuthorVO author) throws Exception {
         authorMapper.authorEnroll(author);
     }
 	
+	/* 작가 목록 */
+    @Override
+    public List<AuthorVO> authorGetList(Criteria cri) throws Exception {
+        log.info("(service)authorGetList()......" + cri);
+    	
+        return authorMapper.authorGetList(cri);
+    }
 }
