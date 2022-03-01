c5da34066b863bcc309f692bb79141259ec53e5b
Park EunChae <78144748+pec6049@users.noreply.github.com>
2022-03-01 14:28:36 +0900

    이미지 출력

--- BookStore/src/main/java/com/bookstore/controller/BookController.java
+++ BookStore/src/main/java/com/bookstore/controller/BookController.java
@@ -1,6 +1,15 @@
 package com.bookstore.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,7 +22,26 @@ public class BookController {
 	@RequestMapping(value="/main", method=RequestMethod.GET)
 	public void mainPageGET() {
 		log.info("메인 페이지 진입");
-
+	}
+	
+	@GetMapping("/display")
+	public ResponseEntity<byte[]> getImage(String fileName) {
+		log.info("getImage()........" + fileName);
+		
+		File file = new File("C:\\Users\\PC\\Desktop\\bookstore-project\\upload\\" + fileName);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			header.add("Content-type", Files.probeContentType(file.toPath()));
+			
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
