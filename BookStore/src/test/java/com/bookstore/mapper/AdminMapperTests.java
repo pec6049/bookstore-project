dd82d2197642148f86ad372fa61bada602b61944
Park EunChae <78144748+pec6049@users.noreply.github.com>
2022-03-14 20:06:50 +0900

    상품 등록

--- BookStore/src/test/java/com/bookstore/mapper/AdminMapperTests.java
+++ BookStore/src/test/java/com/bookstore/mapper/AdminMapperTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.bookstore.model.AttachImageVO;
 import com.bookstore.model.BookVO;
 import com.bookstore.model.Criteria;
 
@@ -19,26 +20,28 @@ public class AdminMapperTests {
 	private AdminMapper mapper;
 	
 	/* 상품 등록 */
-	/*
 	@Test
 	public void bookEnrollTest() throws Exception{
 		
 		BookVO book = new BookVO();
 		
 		book.setBookName("mapper 테스트");
-		book.setAuthorId(123);
+		book.setAuthorId(524);
 		book.setPubleYear("2021-03-18");
 		book.setPublisher("출판사");
-		book.setCateCode("0231");
+		book.setCateCode("100000");
 		book.setBookPrice(20000);
 		book.setBookStock(300);
 		book.setBookDiscount(0.23);
 		book.setBookIntro("책 소개 ");
 		book.setBookContents("책 목차 ");
 		
+		System.out.println("Before BookVO : " + book);
+		
 		mapper.bookEnroll(book);
+		
+		System.out.println("After BookVO : " + book);
 	}
-	*/
 	
 	/* 카테고리 리스트 */
 	/*
@@ -103,6 +106,7 @@ public class AdminMapperTests {
 	*/
 	
 	/* 상품 정보 삭제 */
+	/*
 	@Test
 	public void goodsDeleteTest() {
 		int bookId = 169;
@@ -112,5 +116,20 @@ public class AdminMapperTests {
 			System.out.println("삭제 성공");
 		}
 	}
+	*/
 	
+	/* 이미지 등록 */
+	/*
+	@Test
+	public void imageEnrollTest() {
+		AttachImageVO vo = new AttachImageVO();
+		
+		vo.setBookId(137);
+		vo.setFileName("test");
+		vo.setUploadPath("test");
+		vo.setUuid("test2");
+		
+		mapper.imageEnroll(vo);
+	}
+	*/
 }
