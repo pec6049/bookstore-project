3c6abd606eabf7e7be332457b56c0dc112fb30e1
Park EunChae <78144748+pec6049@users.noreply.github.com>
2022-03-15 20:08:03 +0900

    이미지 정보 반환

--- BookStore/src/main/java/com/bookstore/controller/BookController.java
+++ BookStore/src/main/java/com/bookstore/controller/BookController.java
@@ -3,9 +3,12 @@ package com.bookstore.controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
@@ -13,11 +16,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bookstore.mapper.AttachMapper;
+import com.bookstore.model.AttachImageVO;
+
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 public class BookController {
+	
+	@Autowired
+	private AttachMapper attachMapper;
 
 	@RequestMapping(value="/main", method=RequestMethod.GET)
 	public void mainPageGET() {
@@ -42,6 +51,15 @@ public class BookController {
 			e.printStackTrace();
 		}
 		return result;
+	}
+	
+	/* 이미지 정보 반환 */
+	@GetMapping(value="/getAttachList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<List<AttachImageVO>> getAttachList(int bookId){
+		
+		log.info("getAttachList.........." + bookId);
+		
+		return new ResponseEntity<List<AttachImageVO>>(attachMapper.getAttachList(bookId), HttpStatus.OK);
 	}
 
 }
